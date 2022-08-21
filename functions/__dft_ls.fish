562d48a9f826ca84bb12e64ee549f6c94e9776fe
Thomas Gossmann <gossi@users.noreply.github.com>
2022-08-21 18:20:55 +0200

    c at $DFTFILE

--- functions/__dft_ls.fish
+++ functions/__dft_ls.fish
@@ -1,7 +1,8 @@
 function __dft_ls
     if test -f $DFTFILE
-        while read -la line
-            echo $line
-        end <$DFTFILE
+        #while read -la line
+        #    echo $line
+        #end <$DFTFILE
+        /bin/cat $DFTFILE
     end
 end
