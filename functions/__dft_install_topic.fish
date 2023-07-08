9305ead41309c301f63390f99f3acea879f4ff4f
gossi <gossi@users.noreply.github.com>
2023-07-08 17:38:25 +0200

    Fix installer

--- functions/__dft_install_topic.fish
+++ functions/__dft_install_topic.fish
@@ -1,7 +1,14 @@
 function __dft_install_topic -a topic
-    set -l installer $DFT_HOME/$topic/install.fish
-    if test -f $installer
-        source $installer
+    # does the topic exit?
+    if test -d $DFT_HOME/$topic
+
+        # is there an installer, if so run it
+        set -l installer $DFT_HOME/$topic/install.fish
+        if test -f $installer
+            source $installer
+        end
+
+        # persisting the topic
         __dft_persist_topic $topic
     end
 end
